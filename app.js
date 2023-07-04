d1aad1494f244cbf191769aa33d269d6da3cf2c7
farahhani08 <133956059+farahhani08@users.noreply.github.com>
2023-07-04 13:38:30 +0800

    Update app.js
    changed background env

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ class App{
         
         const self = this;
         
-        loader.load( './assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
+        loader.load( './assets/hdr/beautiful-blue-sky-with-white-clouds.hdr', ( texture ) => {
           const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
           pmremGenerator.dispose();
 
