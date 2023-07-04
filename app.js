62afb4ff6083cfac7118f1aa8b0640b4ff89f9a6
farahhani08 <133956059+farahhani08@users.noreply.github.com>
2023-07-04 13:15:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.add( this.dolly );
         
-		const ambient = new THREE.HemisphereLight(0xFFD700, 0x7A7A7A, 0.7);
+		const ambient = new THREE.HemisphereLight(0xFFD700, 0x7A7A7A, 0.8);
 		this.scene.add(ambient);
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
